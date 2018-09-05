6b61147686d01b5d21e36b17bd76e8b2011341a7
spikar1 <Steffen.fureli@gmail.com>
2018-09-05 10:06:56 +0200

    Made code

---
+++ Solutions/Assets/ThisIsCode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisIsCode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Yes, it is, it is code
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
